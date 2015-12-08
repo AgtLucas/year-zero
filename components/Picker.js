37554f48a24ef20ab96d6f2671a14d3caa071582
Lucas <lucas@agtlucas.com>
2015-12-08 14:02:09 -0200

    Update Picker component

---
+++ components/Picker.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Picker extends Component {
+
+}
+
+Picker.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.string.isRequired
+  ).isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+}
