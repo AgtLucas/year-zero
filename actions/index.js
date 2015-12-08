c702611b2047c103802206267a2274205db908f0
Lucas <lucas@agtlucas.com>
2015-12-08 16:43:10 -0200

    Update index action

--- actions/index.js
+++ actions/index.js
@@ -34,3 +34,31 @@ function receivePosts(reddit, json) {
     receivedAt: Date.now()
   }
 }
+
+function fetchPosts(reddit) {
+  return dispatch => {
+    dispatch(requestPosts(reddit));
+    return fetch(`http://www.reddit.com/r/${reddit}.json`)
+      .then(response => response.json())
+      .then(json => dispatch(receivePosts(reddit, json)))
+  }
+}
+
+function shouldFetchPosts(state, reddit) {
+  const posts = state.postsByReddit[reddit];
+  if (!posts) {
+    return true;
+  }
+  if (posts.isFetching) {
+    return false;
+  }
+  return pots.didInvalidate;
+}
+
+export function fetchPostsIfNeeded(reddit) {
+  return (dispatch, getState) => {
+    if (shouldFetchPosts(getState(), reddit)) {
+      return dispatch(fetchPosts(reddit));
+    }
+  }
+}
